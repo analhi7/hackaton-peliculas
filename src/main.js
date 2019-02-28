d8b7a9378745a1e1a09d05f8ca355b5373f2594d
analhi7 <analhi-e-m7@gmail.com>
2019-02-28 14:14:21 -0600

    Funcion imprimir

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const objPelis=[];
 const firstPage =document.getElementById("first-page")
 //let i=0;
-const arrayPelis =['Get out','Psycho','The babadook','The cabinet of Dr. Caligari','The silence of the Lambs','Girls Trip','Zoolander','I Heart Huckabees','Tenacious D in The Pick of Destiny','Bridesmaids','Indiana Jones','Star Wars','Jumanji','Jurassic Park','Pirates of the Caribbean: The Curse of the Black Pearl','Mad Max: Fury Road','E.T.The Extra-Terrestrial','Metropolis','Gravity','War for the Planet of the Apes','Wall-E','Die Hard','Terminator 2: Judgment Day','The Dark Knight','Drive'];
+const arrayPelis =['Get out','Psycho','The babadook','The cabinet of Dr. Caligari','The silence of the Lambs','Girls Trip','Zoolander','I Heart Huckabees','Tenacious D in The Pick of Destiny','Bridesmaids','Indiana Jones','Star Wars','Jumanji','Jurassic Park','Pirates of the Caribbean: The Curse of the Black Pearl','Mad Max: Fury Road','Metropolis','Gravity','War for the Planet of the Apes','Wall-E','Die Hard','Terminator 2: Judgment Day','The Dark Knight','Drive', 'E.T. The Extra-Terrestrial', 'Seven Samurai'];
 url = 'http://www.omdbapi.com/?t=';
 
 for (let i=0; i< arrayPelis.length; i++)
