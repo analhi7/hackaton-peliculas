777276d1f0e8480294816799e1b177b3aaf53a56
analhi7 <analhi-e-m7@gmail.com>
2019-03-01 08:32:58 -0600

    Actualiza carrusel, falido

---
+++ src/data.js
@@ -0,0 +1,23 @@
+
+window.movies={
+
+
+selectCard: (movieId, objPelis) =>{
+    const character= objPelis.filter(element=> element.title===movieId);
+    return selectCard;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const objPelis=[];
-const firstPage =document.getElementById("first-page")
+
+const cardSection= document.getElementById("card-section")
+const characterFromList = document.getElementsByClassName("nameList");
 //let i=0;
 const arrayPelis =['Get out','Psycho','The babadook','The cabinet of Dr. Caligari',];
 url = 'http://www.omdbapi.com/?t=';
@@ -14,32 +16,64 @@ for (let i=0; i< arrayPelis.length; i++)
    // console.log (data); 
     objPelis.push(data);
     //console.log(objPelis);
-    getMovies(objPelis)
+    const dataMovies =  objPelis;
+    getMovies(dataMovies);
+    return dataMovies;
+    
   }) 
   //console.log (data); 
-  
-  .catch(err => (err))
+  //getMovies(dataMovies)
+  //.catch(err => (err))
 }
 
 
-
-function getMovies(objectPelis) {
+const firstPage =document.getElementById("first-page")
+function getMovies(dataMovies) {
+  console.log(dataMovies);
     //const stringMovies = JSON.parse(localStorage.getItem('data'));
-    const sectionOne = firstPage;
-    sectionOne.innerHTML='';
-    objectPelis.forEach( element => {
-
-      let section = `
-      <div class="nameList"><div class="rolPersonajes"><img class="splashPresentation" src="${element.Poster}"></div></div>
-      
-      ` 
+    //const section = firstPage;
+    firstPage.innerHTML;
+    let section ='';
+   for(let i in dataMovies) {
+    section = section+`
+     
+      <img src="${dataMovies[i].Poster}" class="d-block w-25" alt="...">
+  
+ 
+      `
       firstPage.insertAdjacentHTML("beforeend",section);
-      console.log(sectionOne)
-    }); 
-      
+      return firstPage;
+   }
+
+   console.log(firstPage)     
   
   }
   
+
+
+
+
+  
+
+  // const printCardMovie = (objectPelis)=>{
+  //   firstPage.innerHTML = "";
+  //   objectPelis.forEach(element=>{
+  //     let cardMovie= `
+  //     <div class="nameList"><div class="rolPersonajes"><img class="splashPresentation" src="${element.Poster}"></div></div>
+      
+  //     ` 
+  //     firstPage.insertAdjacentHTML("beforeend",cardMovie);
+     
+  //     for (let i = 0; i < characterFromList.length; i++) {
+  //       characterFromList[i].addEventListener('click', () => {
+  //   console.log('si funciono')
+    
+  //       })
+  //     }
+
+
+  //   })
+  // }
 
     // //console.log(stringPokemon);
     // //objPelis.push(stringMovies);
